9272d807a09dfdaca613807536ec95a744e1b6aa
sanjana20047 <gummadisanjana2004@gmail.com>
2026-05-28 19:28:17 +0530

    Add files via upload
    addition of 64bit numbers

---
+++ add64bit.s
@@ -0,0 +1,14 @@
+		;program to add 64bit numbers
+		;first 32 bit number in R0 and R1
+		;second 32 bit number in R2 and R3
+		;R4 and R5 stores result
+		AREA addition,CODE,READONLY
+		ENTRY
+		LDR R0,=0x11111111
+		LDR R1,=0x22222222
+		LDR R2,=0x33333333
+		LDR R3,=0x44444444
+		ADDS R4,R0,R2
+		ADC R5,R1,R3
+STOP    B   STOP
+		END
